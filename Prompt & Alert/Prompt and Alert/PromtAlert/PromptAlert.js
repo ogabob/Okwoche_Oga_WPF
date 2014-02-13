264224c79e00f7fa39bbf69f4b369017ba532748
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-13 16:44:29 -0500

    Hello Ms. Carroll Here is my Prompt and Alert Assignment. Thank You, Oga Okwoche

---
+++ Prompt & Alert/Prompt and Alert/PromtAlert/PromptAlert.js
@@ -0,0 +1,9 @@
+
+
+//circumference of a circle.
+
+var baseLength= prompt("We are calculating the area of a triangle\nPlease enter the Base Length")
+var height= prompt("Please enter the Height")
+var area= baseLength* height/2
+var statement= "The area of your triangle is "+ area
+alert(statement)
