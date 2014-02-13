98a5e2db1c9215529c36df80b37b849004504a52
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-13 10:45:39 -0500

    no message

---
+++ AssociatingJS/ AssociatingJS/js/script.js
@@ -0,0 +1,110 @@
+alert("Hello World");
+var yearBorn =1959;
+
+console.log (yearBorn);
+
+console.log("Hello World");
+
+var age;
+age = 38;
+var yearOfBirth = 1910;
+
+var isColorRed = false;
+
+var firsNameLastName = "Oga Okwoche"
+
+var phrase = "I can\'t go  there";
+
+var phrase = "paint the wall blue \n if not paint it green"
+
+
+var kindsOfColors =["yellow","blue","orange","green"]// 0,1,2,3
+
+console. log (kindsOfColors)
+console.log ([0]);
+console.log([4]);
+
+//Expressions
+
+var a = 4;
+a = a + 7;
+console.log (a);
+
+//basic arithmetic
+var yearBorn= 1914;
+var newAge = 2014-yearBorn;
+console.log (newAge);
+
+
+//volume of a rectangle is the length time time the width times the height
+
+var length= 4;
+var width=  12;
+var height= 8;
+var volume= length * width * height;
+console.log (volume);
+
+//area of a triangle
+var base= 10
+var triangleHeight= 4;
+var triangleArea= triangleHeight * base/2;
+console.log(triangleArea);
+
+//Experssions- Modulo oprerator
+var remainder = 32 % 6;
+console.log(remainder);
+
+//order of operations
+var x= 4
+var y =3
+var z =14
+var average = (x + y + z)/3
+console.log(average)
+
+//expressions with arrays
+var penPackets= [200, 40, 20]
+var total= penPackets[0]+penPackets[1]+penPackets[2]
+console.log(total);
+
+//concatenating expressions.
+
+var firstName= "Oga";
+var lastName= "Okwoche";
+var fullName= firstName + " " + lastName
+console. log (fullName)
+
+
+//casting
+var integer= "4";
+var a= 9
+var sum= a + Number (integer)
+console. log(sum)
+
+var areaCode =231
+var firstprt = 888
+var secondprt = 4210
+
+phoneNumber = "("+(areaCode)+")" + firstprt+"-" + secondprt
+
+console.log(phoneNumber)
+
+//assignment operators
+var c = 9
+c+= 9
+console.log(c)
+
+var d =10
+d-= 7
+console. log(d)
+
+var n=8
+n*=8
+console.log(n)
+
+var t=2
+t++
+console.log(t)
+
+var w=4
+w--
+console.log(w)
