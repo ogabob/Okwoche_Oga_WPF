72336d96d070ae88f5f34f07dcf131397b8c9fb1
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-21 21:43:24 -0500

    Hello Ms. Carroll, Here is my personal conditional work.I apologize it is coming late

---
+++ Conditionals_Personal/js/Conditional_Personal.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Oga Okwoche on 2/20/14.
+ */
+
+
+//This is a calculation that will decide where what tours my family wil visit when they visit in the summer.
+
+var numberOfPeople= prompt("Please enter the number of family members coming to visit.");
+
+var numberOfDays=prompt("Please enter the number of days family members will be staying");
+
+var budget=prompt("Please enter the budget for the vacation in dollars");
+
+var foodCostPerDay= 30
+var hotelCostPerNight= 20;
+alert("2 people per room");
+alert("The cost of food per day is $30")
+alert("The cost of a room is $20 per night")
+var totalCostFood =   numberOfDays *numberOfPeople*foodCostPerDay;
+ console.log(totalCostFood)//delete this line
+
+var totalHotelCost=  (numberOfDays-1)*numberOfPeople*hotelCostPerNight/2;
+console.log(totalHotelCost)//delete this line..
+
+
+var totalHotelAndFoodCost= totalCostFood+totalHotelCost;
+console.log(totalHotelAndFoodCost)//delete this line
+
+var threshold1=budget/2
+console.log(threshold1)//delete this line
+var threshold2= budget/3
+console.log(threshold2)//delete this line
+
+
+if (totalHotelAndFoodCost>threshold1){
+    console.log("We will go sight seeing in Baltimore City!");
+}else if (totalHotelAndFoodCost<threshold1 && totalHotelAndFoodCost>=threshold2){
+    console.log("We can go to do some shopping at Towson Mall");
+}else(console.log("We will go visit New York and do some shopping!"));
+
+
