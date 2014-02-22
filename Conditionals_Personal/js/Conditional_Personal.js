e0d29a3207ab607752b265ae6371b19cecd2b8fa
ogabob <ogabob11@fullsail.edu>
2014-02-21 21:59:59 -0500

    Update Conditional_Personal.js

--- Conditionals_Personal/js/Conditional_Personal.js
+++ Conditionals_Personal/js/Conditional_Personal.js
@@ -35,7 +35,7 @@ var threshold2= budget/3//this is the safest zone to spend
 if (totalHotelAndFoodCost>threshold1){// this condition applies if the total cost exceeds the maximum safe zone
     console.log("We will go sight seeing in Baltimore City!");//This is the print result if the first condition is true
 }else if (totalHotelAndFoodCost<threshold1 && totalHotelAndFoodCost>=threshold2){
-    console.log("We can go to do some shopping at Towson Mall");
+    console.log("We can go to do some shopping at Towson Mall");//This is the print result if the second condition is true
 }else(console.log("We will go visit New York and do some shopping!"));
 
 
