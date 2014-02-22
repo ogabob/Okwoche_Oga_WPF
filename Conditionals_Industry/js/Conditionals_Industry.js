a2259cfa920a2ac25bec1b6bb65e258fc35150ec
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-21 23:45:07 -0500

    Additional comments were added for further explanation.

--- Conditionals_Industry/js/Conditionals_Industry.js
+++ Conditionals_Industry/js/Conditionals_Industry.js
@@ -7,22 +7,22 @@
 
 var pixelWidth=prompt("Please enter the width in pixel");//This is the pixel width
 
-var pixelHeight=prompt("Please enter the height in pixel");
+var pixelHeight=prompt("Please enter the height in pixel");//This is the Pixel height
 
-var printWidth=prompt("Please enter the Print size width in Inches");
+var printWidth=prompt("Please enter the Print size width in Inches");//This is the width of the print size
 
-var printHeight=prompt ("Please enter the print size height in Inches");
+var printHeight=prompt ("Please enter the print size height in Inches");//This is the pixel height in Inches
 
 var printerDPI=prompt("Please put in the Printer DPI in Pixels");//DPI stands for Dots Per Pixel.
 
-var imagePrintDPI= pixelWidth/printWidth;
+var imagePrintDPI= pixelWidth/printWidth;// This is the minimum Image Print DPI that can give a good print resolution
 
 
 if (imagePrintDPI>=printerDPI){
     (console.log("You can print this Image "));//this is the print result if the image DPI is greater than or equal to the printer DPI
 
 }else if(imagePrintDPI<printerDPI && printerDPI> imagePrintDPI-50){
-    console.log("This image can be printed,but it is not the best.It will be blurry. Please increase the resolution");
+    console.log("This image can be printed,but it is not the best.It will be blurry. Please increase the resolution");// this is the print result if the print DPI is less than the Image Print and less than(Image DPI print-50
 }else{
-    console.log("Sorry,We cannot print this image.Please increase the resolution ")
+    console.log("Sorry,We cannot print this image.Please increase the resolution ")// This is the print result if neither of the first 2 conditions are true.
 }
