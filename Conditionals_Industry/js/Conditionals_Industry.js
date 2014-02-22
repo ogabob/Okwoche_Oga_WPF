3f5bcfe9a194e0ac6fc21a3651ab612d20c92db7
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-21 23:48:59 -0500

    Additional comments were made for further explanation.

--- Conditionals_Industry/js/Conditionals_Industry.js
+++ Conditionals_Industry/js/Conditionals_Industry.js
@@ -18,10 +18,10 @@ var printerDPI=prompt("Please put in the Printer DPI in Pixels");//DPI stands fo
 var imagePrintDPI= pixelWidth/printWidth;// This is the minimum Image Print DPI that can give a good print resolution
 
 
-if (imagePrintDPI>=printerDPI){
+if (imagePrintDPI>=printerDPI){//This condition applies if it is true
     (console.log("You can print this Image "));//this is the print result if the image DPI is greater than or equal to the printer DPI
 
-}else if(imagePrintDPI<printerDPI && printerDPI> imagePrintDPI-50){
+}else if(imagePrintDPI<printerDPI && printerDPI> imagePrintDPI-50){//this condition applies if the first condition is false
     console.log("This image can be printed,but it is not the best.It will be blurry. Please increase the resolution");// this is the print result if the print DPI is less than the Image Print and less than(Image DPI print-50
 }else{
     console.log("Sorry,We cannot print this image.Please increase the resolution ")// This is the print result if neither of the first 2 conditions are true.
