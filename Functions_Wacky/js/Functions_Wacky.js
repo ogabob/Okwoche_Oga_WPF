39d8bfd3004e74386ffe71e65bb049a0a69d704e
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-28 04:02:52 -0500

    additional comments

--- Functions_Wacky/js/Functions_Wacky.js
+++ Functions_Wacky/js/Functions_Wacky.js
@@ -7,9 +7,9 @@ alert("We want to know the kind of shape given the length of sides of the shape"
 
 var a=prompt("Please enter the length of side a")//length in meters
 var b= prompt("Please enter the length of side b")//length in meters
-var c= prompt("please enter the length of side c")
-var d= prompt("please enter the length  of side d")
+var c= prompt("please enter the length of side c")//length in meters
+var d= prompt("please enter the length  of side d")//length in meters
 
- var area=a*b*c*d
+ var area=a*b*c*d// this is the area of the shape
 
- (area/2===a*b/2)? console.log("This is a square"):console.log("you figure out the shape")// the area of a square is 2 times the area of a triangle
+ (area/2===a*b/2)? console.log("This is a square"):console.log("you figure out the shape")// the area of a square is 2 times the area of a triangle.this is a ternary that shows if the condition is either true or false.
