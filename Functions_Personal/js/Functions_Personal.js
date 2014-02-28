673f3b5f2aa3e58f7a6c151a2d7bc60549a94e41
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-27 21:47:39 -0500

    Additional comments were added to explain the conditions and the print result.

--- Functions_Personal/js/Functions_Personal.js
+++ Functions_Personal/js/Functions_Personal.js
@@ -19,20 +19,20 @@ var finalStatement=CalcApr(n,tf,ab,T);// These are the arguments with the variab
 function CalcApr(n,tf,ab,T){//This is the parameter of the function where the arguments are stored.
    var apr= (2* n * tf )/( ab *T+1)// This is  the basic formula for calculating the APR.
    var statement1=("Your APR is "+apr+ ". It is not recommended that you take this loan")//This is a variable statement if certain conditions are true
-   var statement2=("Your APR is "+apr+ ". You can take the loan if you have a down payment")
-   var statement3=("Your APR is "+apr+ ". You can go ahead and take the loan. No down payment is necessary")
-    if (apr>5){
-       return statement1
+   var statement2=("Your APR is "+apr+ ". You can take the loan if you have a down payment")//This is a variable statement if certain conditions are true
+   var statement3=("Your APR is "+apr+ ". You can go ahead and take the loan. No down payment is necessary")//This is a variable statement if certain conditions are true
+    if (apr>5){//first condition
+       return statement1// statement1 is true if only this condition is true and is pushed out of the function
 
-    }else if(apr<5 && apr>0.9) {
-       return statement2
+    }else if(apr<5 && apr>0.9) {//second condition
+       return statement2//statement2 is true if only this condition is true and is pushed out of the function
 
     }else{
-       return statement3
+       return statement3//if statement1 and statement2 are false, then only statement3 is true and is pushed out of the function.
     }
 
 
 
 }
 
-console.log(finalStatement)
+console.log(finalStatement)//This is the final print result of the function after the conditions have been satisfied.
