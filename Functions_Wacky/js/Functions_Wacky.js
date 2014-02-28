3cefc0b4f193c5344c187726014e31ebb5c76142
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-28 04:07:08 -0500

    functions were added for proper formatting.

--- Functions_Wacky/js/Functions_Wacky.js
+++ Functions_Wacky/js/Functions_Wacky.js
@@ -3,13 +3,13 @@
  */
 //we will be deciding what kind of shape given the length of sides.
 
-alert("We want to know the kind of shape given the length of sides of the shape")//it is easy to know if a specific shape is a square following the conditions below.
+alert("We want to know the kind of shape given the length of sides of the shape");//it is easy to know if a specific shape is a square following the conditions below.
 
-var a=prompt("Please enter the length of side a")//length in meters
-var b= prompt("Please enter the length of side b")//length in meters
-var c= prompt("please enter the length of side c")//length in meters
-var d= prompt("please enter the length  of side d")//length in meters
+var a=prompt("Please enter the length of side a");//length in meters
+var b= prompt("Please enter the length of side b");//length in meters
+var c= prompt("please enter the length of side c");//length in meters
+var d= prompt("please enter the length  of side d");//length in meters
 
- var area=a*b*c*d// this is the area of the shape
+ var area=a*b*c*d;// this is the area of the shape
 
- (area/2===a*b/2)? console.log("This is a square"):console.log("you figure out the shape")// the area of a square is 2 times the area of a triangle.this is a ternary that shows if the condition is either true or false. it looks like ternaries do not work in functions. I will have to ask Ms Carroll about that.
+ (area/2===a*b/2)? console.log("This is a square"):console.log("you figure out the shape");// the area of a square is 2 times the area of a triangle.this is a ternary that shows if the condition is either true or false. it looks like ternaries do not work in functions. I will have to ask Ms Carroll about that.
