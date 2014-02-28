5998bbfc2057f78545ce18eeecacee1f3c18a9e3
Oga Okwoche <ogabob11@fullsail.edu>
2014-02-27 21:35:28 -0500

    Additional Comments were added to explain the code.

--- Functions_Personal/js/Functions_Personal.js
+++ Functions_Personal/js/Functions_Personal.js
@@ -5,14 +5,14 @@
 
 
 // I will be deciding is I will be taking an auto loan by deciding the APR. APR stands for the Annual Percentage Rate
-alert("We will be deciding if we will take the bank loan for a car based on the APR ")
+alert("We will be deciding if we will take the bank loan for a car based on the APR ")//This alert statement is an introduction to the user to understand what the user will be doing.
 var n= prompt("Please enter the number of payments you will be making in 1 year")//This could range from 1-12
 
 var tf=prompt("Please enter the total financing charges in Dollars")// This is the dollar amount of financial charges.
 
 var ab=prompt("Please enter the amount borrowed")//This is the amount borrowed from the bank.
 
-var T=prompt("Please enter the number of scheduled payments for the life of the loan")
+var T=prompt("Please enter the number of scheduled payments for the life of the loan")// This is the total number of payments for the life of the loan
 
 var finalStatement=CalcApr(n,tf,ab,T);
 
